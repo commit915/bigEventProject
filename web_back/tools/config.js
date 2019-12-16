61ea9a745db434aaead569af59a6a5ee60916e1a
cp <1432737544@qq.com>
2019-12-16 16:28:05 +0800

    加了相关注释

--- web_back/tools/config.js
+++ web_back/tools/config.js
@@ -1,3 +1,7 @@
+//使用config.js统一管理接口地址
+//后期维护时，接口地址变化可以在config.js中统一修改，无需到每个html文件中操作
+// 通过全大写、使用_连接的变量名进行接口地址管理
+
 var BASE_URL = "http://localhost:8000";
 var LOGIN = BASE_URL + "/admin/login";
 var LOGOUT = BASE_URL + "/admin/logout";
--- web_back/tools/user.js
+++ web_back/tools/user.js
@@ -1,6 +1,11 @@
+//创建一个user对象,在这里放各种方法,为了语义化
+//使用user.js统一管理用户接口的ajax操作
+//后期维护时，接口使用方式变化可以在user.js中管理
 var user = {
+    //1.login方法:传入一个options对象
     login: function (options) {
         $.ajax({
+            //!!!这里的LOGIN千万不能加"",这里是用的config.js当中设置好的地址
             url: LOGIN,
             type: "POST",
             data: {
@@ -9,8 +14,7 @@ var user = {
             },
             success: function (res) {
                 if (res.code === 200) {
-                    console.log(res);
-
+                    // console.log(res);
                     options.success();
                 } else {
                     options.fail();
@@ -24,6 +28,7 @@ var user = {
             url: LOGOUT,
             type: "POST",
             success: function (res) {
+                console.log(res);
                 if (res.code === 200) {
                     options.success();
                 } else {
